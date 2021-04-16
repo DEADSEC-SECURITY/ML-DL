bd1dadc47ee272a630a6f37a64aa82ae9dd4b784
Antonio Goncalves <amng835@gmail.com>
2021-04-17 00:05:42 +0100

    Optimized Model and added Checkpoints

--- Dog VS Cat Classification/Dog Vs Cat Train.py
+++ Dog VS Cat Classification/Dog Vs Cat Train.py
@@ -1,9 +1,11 @@
+import datetime
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import pathlib
 
 DATA = 'Data/'
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 VAL_SPLIT = 0.2
 
 DATA = pathlib.Path(DATA)
@@ -12,32 +14,44 @@ train = tf.keras.preprocessing.image_dataset_from_directory(
     DATA,
     validation_split=VAL_SPLIT,
     subset='training',
-    seed=3391283,
+    seed=3,
     image_size=(200, 200),
-    batch_size=BATCH_SIZE
+    batch_size=BATCH_SIZE,
+    color_mode='rgb',
 )
 
 val = tf.keras.preprocessing.image_dataset_from_directory(
     DATA,
     validation_split=VAL_SPLIT,
     subset='validation',
-    seed=3391283,
+    seed=3,
     image_size=(200, 200),
-    batch_size=BATCH_SIZE
+    batch_size=BATCH_SIZE,
+    color_mode='rgb',
 )
 
 model = tf.keras.Sequential([
+    tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
+    tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+    tf.keras.layers.experimental.preprocessing.RandomRotation(.1),
     tf.keras.layers.Conv2D(32, (3, 3), strides=2, activation='relu', padding='same', kernel_initializer='he_uniform'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(32, (3, 3), strides=2, activation='relu', padding='same', kernel_initializer='he_uniform'),
+    tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Conv2D(64, (3, 3), strides=2, activation='relu', padding='same', kernel_initializer='he_uniform'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(64, (3, 3), strides=2, activation='relu', padding='same', kernel_initializer='he_uniform'),
+    tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Conv2D(128, (3, 3), strides=2, activation='relu', padding='same', kernel_initializer='he_uniform'),
+    tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(512, activation='relu', kernel_initializer='he_uniform'),
+    tf.keras.layers.Dense(1024, activation='relu', kernel_initializer='he_uniform'),
     tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(512, activation='relu', kernel_initializer='he_uniform'),
+    tf.keras.layers.Dense(1024, activation='relu', kernel_initializer='he_uniform'),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(2, activation='softmax')
 ])
@@ -48,20 +62,15 @@ model.compile(optimizer=optimizer,
               loss=tf.losses.SparseCategoricalCrossentropy(),
               metrics=['accuracy'])
 
+log_dir = f'Logs/fit/' + '20210413-124250'#datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
 fitted_model = model.fit(
     train,
     validation_data=val,
-    epochs=20
+    epochs=20,
+    use_multiprocessing=True,
+    callbacks=[tensorboard_callback, tf.keras.callbacks.ModelCheckpoint("Models/Checkpoint/save_at_{epoch}.h5")],
 )
 
 model.save(f'Models/dog_vs_cat.h5')
-
-plt.title('Loss')
-plt.plot(fitted_model.history['loss'], label='loss')
-plt.plot(fitted_model.history['val_loss'], label='val_loss')
-plt.show()
-
-plt.title('Accuracy')
-plt.plot(fitted_model.history['accuracy'], label='acc')
-plt.plot(fitted_model.history['val_accuracy'], label='val_acc')
-plt.show()
